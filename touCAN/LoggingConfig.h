c8a02bb6a6b70c8ba5a13b6419068111f8357a79
hgrasman <henrygrasman@gmail.com>
2024-05-22 16:32:04 -0400

    New logging helper paths

--- touCAN/LoggingConfig.h
+++ touCAN/LoggingConfig.h
@@ -1,5 +1,5 @@
 
-//Generated 2024-05-22 11:10:50.721836 with logging_helper_2.py for EV Kartz Kettering University
+//Generated 2024-05-22 16:31:29.334100 with logging_helper_2.py for EV Kartz Kettering University
 //Henry Grasman
 
 #ifndef LOGGING_CONFIG
@@ -13,6 +13,10 @@ uint8_t flushCounter = 0;
 
 struct loggingData{
   double LeSDLR_t_currentTime;
+  double LeSDLR_tq_CAN0_TorqueRequest;
+  double LeSDLR_tq_CAN1_TorqueRequest;
+  double LeSDLR_phi_SteeringWheelAngle;
+  double LeSDLR_p_PedalPosition;
   double LeSDLR_tq_TotalTorqueDesired;
   double LeSDLR_P_CombinedMaxPower;
   double LeSDLR_P_CombinedMinPower;
@@ -29,6 +33,7 @@ struct loggingData{
   double LeSDLR_deg_GPSLongitude;
   double LeSDLR_m_GPSAltitude;
   double LeSDLR_deg_GPSHeading;
+  double LeSDLR_deg_GPSMagVariation;
   double LeSDLR_mps_GPSSpeed;
   double LeSDLR_n_GPSSatellites;
   double LeSDLR_e_GPSFixQuality;
@@ -46,6 +51,10 @@ inline void logging_flush_buffer(File logfile){
 
 inline bool logging_queue_data(void){
   loggingMessage.LeSDLR_t_currentTime = (double)esp_timer_get_time() / 1000000.0;
+  loggingMessage.LeSDLR_tq_CAN0_TorqueRequest = VeVDKR_tq_CAN0_TorqueRequest.getValue();
+  loggingMessage.LeSDLR_tq_CAN1_TorqueRequest = VeVDKR_tq_CAN1_TorqueRequest.getValue();
+  loggingMessage.LeSDLR_phi_SteeringWheelAngle = VeVDKR_phi_SteeringWheelAngle.getValue();
+  loggingMessage.LeSDLR_p_PedalPosition = VeVDKR_p_PedalPosition.getValue();
   loggingMessage.LeSDLR_tq_TotalTorqueDesired = VeVDKR_tq_TotalTorqueDesired.getValue();
   loggingMessage.LeSDLR_P_CombinedMaxPower = VeVDKR_P_CombinedMaxPower.getValue();
   loggingMessage.LeSDLR_P_CombinedMinPower = VeVDKR_P_CombinedMinPower.getValue();
@@ -62,6 +71,7 @@ inline bool logging_queue_data(void){
   loggingMessage.LeSDLR_deg_GPSLongitude = VeGPSR_deg_GPSLongitude.getValue();
   loggingMessage.LeSDLR_m_GPSAltitude = VeGPSR_m_GPSAltitude.getValue();
   loggingMessage.LeSDLR_deg_GPSHeading = VeGPSR_deg_GPSHeading.getValue();
+  loggingMessage.LeSDLR_deg_GPSMagVariation = VeGPSR_deg_GPSMagVariation.getValue();
   loggingMessage.LeSDLR_mps_GPSSpeed = VeGPSR_mps_GPSSpeed.getValue();
   loggingMessage.LeSDLR_n_GPSSatellites = VeGPSR_n_GPSSatellites.getValue();
   loggingMessage.LeSDLR_e_GPSFixQuality = VeGPSR_e_GPSFixQuality.getValue();
@@ -72,6 +82,10 @@ inline bool logging_queue_data(void){
 
 inline void logging_write_header(File logfile){
   logfile.print("LeSDLR_t_currentTime");
+  logfile.print(", VeVDKR_tq_CAN0_TorqueRequest");
+  logfile.print(", VeVDKR_tq_CAN1_TorqueRequest");
+  logfile.print(", VeVDKR_phi_SteeringWheelAngle");
+  logfile.print(", VeVDKR_p_PedalPosition");
   logfile.print(", VeVDKR_tq_TotalTorqueDesired");
   logfile.print(", VeVDKR_P_CombinedMaxPower");
   logfile.print(", VeVDKR_P_CombinedMinPower");
@@ -88,6 +102,7 @@ inline void logging_write_header(File logfile){
   logfile.print(", VeGPSR_deg_GPSLongitude");
   logfile.print(", VeGPSR_m_GPSAltitude");
   logfile.print(", VeGPSR_deg_GPSHeading");
+  logfile.print(", VeGPSR_deg_GPSMagVariation");
   logfile.print(", VeGPSR_mps_GPSSpeed");
   logfile.print(", VeGPSR_n_GPSSatellites");
   logfile.print(", VeGPSR_e_GPSFixQuality");
@@ -98,6 +113,10 @@ inline void logging_write_header(File logfile){
 
 inline void logging_write_line(File logfile, struct loggingData *pdataToLog){
   logfile.print(pdataToLog->LeSDLR_t_currentTime, 4);
+  logfile.print(", "); logfile.print(pdataToLog->LeSDLR_tq_CAN0_TorqueRequest, 2);
+  logfile.print(", "); logfile.print(pdataToLog->LeSDLR_tq_CAN1_TorqueRequest, 2);
+  logfile.print(", "); logfile.print(pdataToLog->LeSDLR_phi_SteeringWheelAngle, 1);
+  logfile.print(", "); logfile.print(pdataToLog->LeSDLR_p_PedalPosition, 3);
   logfile.print(", "); logfile.print(pdataToLog->LeSDLR_tq_TotalTorqueDesired, 2);
   logfile.print(", "); logfile.print(pdataToLog->LeSDLR_P_CombinedMaxPower, 0);
   logfile.print(", "); logfile.print(pdataToLog->LeSDLR_P_CombinedMinPower, 0);
@@ -114,6 +133,7 @@ inline void logging_write_line(File logfile, struct loggingData *pdataToLog){
   logfile.print(", "); logfile.print(pdataToLog->LeSDLR_deg_GPSLongitude, 10);
   logfile.print(", "); logfile.print(pdataToLog->LeSDLR_m_GPSAltitude, 4);
   logfile.print(", "); logfile.print(pdataToLog->LeSDLR_deg_GPSHeading, 2);
+  logfile.print(", "); logfile.print(pdataToLog->LeSDLR_deg_GPSMagVariation, 2);
   logfile.print(", "); logfile.print(pdataToLog->LeSDLR_mps_GPSSpeed, 2);
   logfile.print(", "); logfile.print(pdataToLog->LeSDLR_n_GPSSatellites, 0);
   logfile.print(", "); logfile.print(pdataToLog->LeSDLR_e_GPSFixQuality, 0);
